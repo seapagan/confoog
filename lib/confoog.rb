a084120125876800b6374751eeed43ce7908f159
Seapagan <seapagan@gmail.com>
2015-08-25 09:32:12 +0100

    Basic class creation, initialisation and Rspec tests
    Signed-off-by: Seapagan <seapagan@gmail.com>

--- lib/confoog.rb
+++ lib/confoog.rb
@@ -1,5 +1,16 @@
 require "confoog/version"
 
 module Confoog
-  # Your code goes here...
+
+  DEFAULT_CONFIG = '.confoog'
+
+  class Settings
+
+    attr_accessor :config_filename, :config_location
+
+    def initialize(options = {})
+      @config_location = options[:location] || '~/'
+      @config_filename = options[:filename] || DEFAULT_CONFIG
+    end
+  end
 end
--- spec/confoog_spec.rb
+++ spec/confoog_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
 
 describe Confoog do
+
+  subject {Confoog::Settings}
+
   it 'has a version number' do
     expect(Confoog::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'should accept no options when created but should set sensible defaults' do
+    settings=subject.new
+    expect(settings).not_to be nil
+    expect(settings.config_filename).to be Confoog::DEFAULT_CONFIG
+    expect(settings.config_location).to eq '~/'
   end
+
+  it 'should also accept the location and filename on creation, either separate or together' do
+    settings=subject.new(location: '/home/user/configs')
+    expect(settings.config_location).to eq '/home/user/configs'
+    settings=subject.new(filename: '.configfile')
+    expect(settings.config_filename).to eq '.configfile'
+    settings=subject.new(filename: '.myconfigfile', location: '/put/it/here')
+    expect(settings.config_filename).to eq '.myconfigfile'
+    expect(settings.config_location).to eq '/put/it/here'
+  end
+  
 end
