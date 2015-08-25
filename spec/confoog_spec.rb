843c13829eac83eef3e2c3c1e2ac68a745c65874
Seapagan <seapagan@gmail.com>
2015-08-25 14:20:24 +0100

    Refactor existing tests to improve readability
    Signed-off-by: Seapagan <seapagan@gmail.com>

--- spec/confoog_spec.rb
+++ spec/confoog_spec.rb
@@ -8,22 +8,31 @@ describe Confoog do
     expect(Confoog::VERSION).not_to be nil
   end
 
-  it 'should accept no options when created but should set sensible defaults' do
-    settings=subject.new
-    expect(settings).to be_an_instance_of subject
-    expect(settings.config_filename).to be Confoog::DEFAULT_CONFIG
-    expect(settings.config_location).to eq '~/'
+  context "When created with no parameters" do
+    let(:settings) {subject.new}
+    it 'should create the class with no errors' do
+      expect(settings).to be_an_instance_of subject
+    end
+    it 'should set sensible defaults' do
+      #settings=subject.new
+      expect(settings.config_filename).to be Confoog::DEFAULT_CONFIG
+      expect(settings.config_location).to eq '~/'
+    end
+  end
+  context "When created with parameters" do
+    it 'should accept a location for the configuration file' do
+      settings=subject.new(location: '/home/user/configs')
+      expect(settings.config_location).to eq '/home/user/configs'
+    end
+    it 'should accept a filename for the configuration file' do
+      settings=subject.new(filename: '.configfile')
+      expect(settings.config_filename).to eq '.configfile'
+    end
+    it 'should accept both location and filename' do
+      settings=subject.new(filename: '.myconfigfile', location: '/put/it/here')
+      expect(settings.config_filename).to eq '.myconfigfile'
+      expect(settings.config_location).to eq '/put/it/here'
+    end
   end
 
-  it 'should also accept the location and filename on creation, either separate or together' do
-    settings=subject.new(location: '/home/user/configs')
-    expect(settings.config_location).to eq '/home/user/configs'
-    settings=subject.new(filename: '.configfile')
-    expect(settings.config_filename).to eq '.configfile'
-    settings=subject.new(filename: '.myconfigfile', location: '/put/it/here')
-    expect(settings.config_filename).to eq '.myconfigfile'
-    expect(settings.config_location).to eq '/put/it/here'
-  end
-
-  
 end
