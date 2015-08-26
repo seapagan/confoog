c3d73e02e7f6519411dfb50f2cf2a800e4ec5a49
Seapagan <seapagan@gmail.com>
2015-08-26 09:33:25 +0100

    Add fakefs setup to spec/spec_helper.rb
    Signed-off-by: Seapagan <seapagan@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'confoog'
+require 'fakefs/spec_helpers'
+
+RSpec.configure do |config|
+  config.include FakeFS::SpecHelpers, fakefs: true
+end
