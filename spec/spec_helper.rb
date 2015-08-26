0a685614d6a53e187486e6f117081c1cab658314
Seapagan <seapagan@gmail.com>
2015-08-26 11:32:24 +0100

    Add 'pp' gem as require - fix FakeFS issue
    Fixes issue https://github.com/defunkt/fakefs/issues/99
    
    Signed-off-by: Seapagan <seapagan@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'pp' # work around https://github.com/defunkt/fakefs/issues/215
 require 'confoog'
 require 'fakefs/spec_helpers'
 
