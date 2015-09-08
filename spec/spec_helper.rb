b19af090bf29f551a7bec468439a6a1ae92202ce
Seapagan <seapagan@gmail.com>
2015-09-08 14:15:34 +0100

    Tidy up some code smells
    Signed-off-by: Seapagan <seapagan@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,11 +12,6 @@ require 'pp' # work around https://github.com/defunkt/fakefs/issues/99
 require 'confoog'
 require 'fakefs/spec_helpers'
 
-def show_file(filename)
-  contents = File.open(filename, 'r') { |file| file.read }
-  puts contents
-end
-
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers, fakefs: true
 end
