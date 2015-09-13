d52560f888e7cb022f2c48d8ab7577b37bdb0de7
Seapagan <seapagan@gmail.com>
2015-09-13 03:56:56 +0100

    v2.0.1

--- lib/confoog/version.rb
+++ lib/confoog/version.rb
@@ -1,6 +1,6 @@
 # Define the Gem version string
 module Confoog
-  # Version of this Gem, using Semantic Versioning 2.0.0
+  # Version of this Gem, using Semantic Versioning 2.0.1
   # http://semver.org/
   VERSION = '0.4.1'
 end
